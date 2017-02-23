76db69df87ef6da3afa3864c2af1f12652d3c148
Alex Fang <alexfang@heytz.com>
2017-02-23 16:42:48 +0800

    remove reference

--- src/android/MTKWrapper.java
+++ src/android/MTKWrapper.java
@@ -4,18 +4,6 @@ import android.content.Context;
 import android.os.Handler;
 import android.util.Log;
 
-import com.heytz.ESPWrapper.esptouch.protocol.EsptouchGenerator;
-import com.heytz.ESPWrapper.esptouch.task.IEsptouchGenerator;
-import com.heytz.ESPWrapper.esptouch.util.EspNetUtil;
-import com.heytz.ESPWrapper.esptouch.udp.UDPSocketClient;
-import com.heytz.ESPWrapper.esptouch.udp.UDPSocketServer;
-import com.heytz.ESPWrapper.esptouch.task.IEsptouchTaskParameter;
-import com.heytz.ESPWrapper.esptouch.task.EsptouchTaskParameter;
-import com.heytz.ESPWrapper.esptouch.util.ByteUtil;
-import com.heytz.ESPWrapper.esptouch.IEsptouchResult;
-import com.heytz.ESPWrapper.esptouch.EsptouchResult;
-import com.heytz.ESPWrapper.esptouch.IEsptouchListener;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -52,9 +40,7 @@ public class ESPWrapper extends CordovaPlugin {
     private String wifiSSID;
     private String ip;
     private String wifiKey;
-    private UDPSocketClient mSocketClient;
-    private UDPSocketServer mSocketServer;
-    private IEsptouchTaskParameter mParameter;
+
     private volatile boolean mIsInterrupt = false;
     private volatile List<IEsptouchResult> mEsptouchResultList;
     private volatile boolean mIsSuc = false;
@@ -109,53 +95,7 @@ public class ESPWrapper extends CordovaPlugin {
 
             InetAddress localInetAddress = EspNetUtil.getLocalInetAddress(context);
 
-            mParameter = new EsptouchTaskParameter();
-            mEsptouchResultList = new ArrayList<IEsptouchResult>();
-            mSocketClient = new UDPSocketClient();
-            mSocketServer = new UDPSocketServer(mParameter.getPortListening(),
-                    mParameter.getWaitUdpTotalMillisecond(), context);
 
-            IEsptouchGenerator generator = new EsptouchGenerator(wifiSSID, wifiSSID,
-                    wifiKey, localInetAddress, true);
-            listenAsyn(mParameter.getEsptouchResultTotalLen());
-
-            execute(generator);
-
-//            byte[][] gcBytes2;
-//            byte[][] dcBytes2;
-//            GuideCode gc = new GuideCode();
-//            char[] gcU81 = gc.getU8s();
-//            gcBytes2 = new byte[gcU81.length][];
-//
-//            for (int i = 0; i < gcBytes2.length; i++) {
-//                gcBytes2[i] = ByteUtil.genSpecBytes(gcU81[i]);
-//            }
-//
-//            // generate data code
-//            DatumCode dc = new DatumCode(apSsid, apBssid, apPassword, inetAddress,
-//                    isSsidHiden);
-//            char[] dcU81 = dc.getU8s();
-//            dcBytes2 = new byte[dcU81.length][];
-//
-//            for (int i = 0; i < dcBytes2.length; i++) {
-//                dcBytes2[i] = ByteUtil.genSpecBytes(dcU81[i]);
-//            }
-//
-//
-//            mSocketClient.sendData(dcBytes2, 0, dcBytes2.length,
-//                    mParameter.getTargetHostname(),
-//                    mParameter.getTargetPort(),
-//                    mParameter.getIntervalDataCodeMillisecond());
-//
-//
-//            mHandler.postDelayed(timeoutRun, 40000L);
-//            send(wifiSSID, wifiKey);
-//            // todo: replace with EasylinkAPI
-//            //ftcService = new FTC_Service();
-////            SmartConfigActivity.onConfigResult = callbackContext;
-//            ESPCallbackContext = callbackContext;
-//            //ftcListener = new FTCLisenerExtension(callbackContext);
-////            this.transmitSettings(wifiSSID, wifiKey);
             return true;
         }
         if (action.equals("startSocket")) {
@@ -186,111 +126,5 @@ public class ESPWrapper extends CordovaPlugin {
         return true;
     }
 
-    private void listenAsyn(final int expectDataLen) {
-        new Thread() {
-            public void run() {
 
-                long startTimestamp = System.currentTimeMillis();
-                byte[] apSsidAndPassword = ByteUtil.getBytesByString(wifiSSID
-                        + wifiKey);
-                byte expectOneByte = (byte) (apSsidAndPassword.length + 9);
-
-                byte receiveOneByte = -1;
-                byte[] receiveBytes = null;
-                while (mEsptouchResultList.size() < mParameter
-                        .getExpectTaskResultCount() && !mIsInterrupt) {
-                    receiveBytes = mSocketServer
-                            .receiveSpecLenBytes(expectDataLen);
-                    if (receiveBytes != null) {
-                        receiveOneByte = receiveBytes[0];
-                    } else {
-                        receiveOneByte = -1;
-                    }
-                    if (receiveOneByte == expectOneByte) {
-
-                        // change the socket's timeout
-                        long consume = System.currentTimeMillis()
-                                - startTimestamp;
-                        int timeout = (int) (mParameter
-                                .getWaitUdpTotalMillisecond() - consume);
-                        if (timeout < 0) {
-
-                            break;
-                        } else {
-
-                            mSocketServer.setSoTimeout(timeout);
-
-                            if (receiveBytes != null) {
-                                String bssid = ByteUtil.parseBssid(
-                                        receiveBytes,
-                                        mParameter.getEsptouchResultOneLen(),
-                                        mParameter.getEsptouchResultMacLen());
-                                InetAddress inetAddress = EspNetUtil
-                                        .parseInetAddr(
-                                                receiveBytes,
-                                                mParameter.getEsptouchResultOneLen()
-                                                        + mParameter.getEsptouchResultMacLen(),
-                                                mParameter.getEsptouchResultIpLen());
-                                __putEsptouchResult(true, bssid, inetAddress);
-                            }
-                        }
-                    } else {
-
-                    }
-                }
-                mIsSuc = mEsptouchResultList.size() >= mParameter
-                        .getExpectTaskResultCount();
-                interrupt();
-
-            }
-        }.start();
-    }
-    private void __putEsptouchResult(boolean isSuc, String bssid,
-                                     InetAddress inetAddress) {
-        synchronized (mEsptouchResultList) {
-            // check whether the result receive enough UDP response
-            boolean isTaskSucCountEnough = false;
-            Integer count = mBssidTaskSucCountMap.get(bssid);
-            if (count == null) {
-                count = 0;
-            }
-            ++count;
-
-            mBssidTaskSucCountMap.put(bssid, count);
-            isTaskSucCountEnough = count >= mParameter
-                    .getThresholdSucBroadcastCount();
-            if (!isTaskSucCountEnough) {
-
-                return;
-            }
-            // check whether the result is in the mEsptouchResultList already
-            boolean isExist = false;
-            for (IEsptouchResult esptouchResultInList : mEsptouchResultList) {
-                if (esptouchResultInList.getBssid().equals(bssid)) {
-                    isExist = true;
-                    break;
-                }
-            }
-            // only add the result who isn't in the mEsptouchResultList
-            if (!isExist) {
-
-                final IEsptouchResult esptouchResult = new EsptouchResult(isSuc,
-                        bssid, inetAddress);
-                mEsptouchResultList.add(esptouchResult);
-                if (mEsptouchListener != null) {
-                    mEsptouchListener.onEsptouchResultAdded(esptouchResult);
-                }
-            }
-        }
-    }
-
-    public synchronized void interrupt() {
-        if (!mIsInterrupt) {
-            mIsInterrupt = true;
-            mSocketClient.interrupt();
-            mSocketServer.interrupt();
-            // interrupt the current Thread which is used to wait for udp response
-            Thread.currentThread().interrupt();
-        }
-    }
 }
